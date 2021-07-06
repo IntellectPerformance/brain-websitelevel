445dfd82590c9630ded74286ae1995a4b658f9c7
Zenithican <41877233+SatisfactionEntity@users.noreply.github.com>
2021-07-06 18:13:19 +0200

    Removed Caprihotel

--- templates/levelreward.php
+++ templates/levelreward.php
@@ -7,241 +7,11 @@ include_once 'includes/header_scripts.php';
 </head>
 <style>
 
-.alert>.start-icon {
-    margin-right: 0;
-    min-width: 20px;
-    text-align: center;
-}
-
-.alert>.start-icon {
-    margin-right: 5px;
-}
-
-.greencross
-{
-  font-size:18px;
-      color: #25ff0b;
-    text-shadow: none;
-}
-
-.alert-simple.alert-success
-{
-  border: 1px solid rgba(36, 241, 6, 0.46);
-    background-color: rgba(7, 149, 66, 0.12156862745098039);
-    box-shadow: 0px 0px 2px #259c08;
-    color: #0ad406;
-  text-shadow: 2px 1px #00040a;
-  transition:0.5s;
-  cursor:pointer;
-}
-.alert-success:hover{
-  background-color: rgba(7, 149, 66, 0.35);
-  transition:0.5s;
-}
-.alert-simple.alert-info
-{
-  border: 1px solid #0000;
-    background-color: white;
-    box-shadow: 0px 0px 2px #0396ff;
-    color: gray;
-  transition:0.5s;
-  cursor:pointer;
-}
-
-.blue-cross
-{
-  font-size: 18px;
-    color: black;
-    text-shadow: none;
-}
-
-.alert-simple.alert-warning
-{
-      border: 1px solid rgba(241, 142, 6, 0.81);
-    background-color: rgba(220, 128, 1, 0.16);
-    box-shadow: 0px 0px 2px #ffb103;
-    color: #ffb103;
-    text-shadow: 2px 1px #00040a;
-  transition:0.5s;
-  cursor:pointer;
-}
-
-.alert-warning:hover{
-  background-color: rgba(220, 128, 1, 0.33);
-  transition:0.5s;
-}
-
-.warning
-{
-      font-size: 18px;
-    color: #ffb40b;
-    text-shadow: none;
-}
-
-.alert-simple.alert-danger
-{
-  border: 1px solid rgba(241, 6, 6, 0.81);
-    background-color: rgba(220, 17, 1, 0.16);
-    box-shadow: 0px 0px 2px #ff0303;
-    color: #ff0303;
-    text-shadow: 2px 1px #00040a;
-  transition:0.5s;
-  cursor:pointer;
-}
-
-.alert-danger:hover
-{
-     background-color: rgba(220, 17, 1, 0.33);
-  transition:0.5s;
-}
-
-.danger
-{
-      font-size: 18px;
-    color: #ff0303;
-    text-shadow: none;
-}
-
-.alert-simple.alert-primary
-{
-  border: 1px solid rgba(6, 241, 226, 0.81);
-    background-color: rgba(1, 204, 220, 0.16);
-    box-shadow: 0px 0px 2px #03fff5;
-    color: #03d0ff;
-    text-shadow: 2px 1px #00040a;
-  transition:0.5s;
-  cursor:pointer;
-}
-
-.alert-primary:hover{
-  background-color: rgba(1, 204, 220, 0.33);
-   transition:0.5s;
-}
-
-.alertprimary
-{
-      font-size: 18px;
-    color: #03d0ff;
-    text-shadow: none;
-}
-
-.square_box {
-    position: absolute;
-    -webkit-transform: rotate(45deg);
-    -ms-transform: rotate(45deg);
-    transform: rotate(45deg);
-    border-top-left-radius: 45px;
-    opacity: 0.302;
-}
-
-.square_box.box_three {
-    background-image: -moz-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
-    background-image: -webkit-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
-    background-image: -ms-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
-    opacity: 0.059;
-    left: -80px;
-    top: -60px;
-    width: 500px;
-    height: 500px;
-    border-radius: 45px;
-}
-
-.square_box.box_four {
-    background-image: -moz-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
-    background-image: -webkit-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
-    background-image: -ms-linear-gradient(-90deg, #290a59 0%, #3d57f4 100%);
-    opacity: 0.059;
-    left: 150px;
-    top: -25px;
-    width: 550px;
-    height: 550px;
-    border-radius: 45px;
-}
-
-.alert:before {
-    content: '';
-    position: absolute;
-    width: 0;
-    height: calc(100% - 44px);
-    border-left: 1px solid;
-    border-right: 2px solid;
-    border-bottom-right-radius: 3px;
-    border-top-right-radius: 3px;
-    left: 0;
-    top: 50%;
-    transform: translate(0,-50%);
-      height: 20px;
-}
-
-.fa-times
-{
--webkit-animation: blink-1 2s infinite both;
-	        animation: blink-1 2s infinite both;
-}
-
-
-/**
- * ----------------------------------------
- * animation blink-1
- * ----------------------------------------
- */
-@-webkit-keyframes blink-1 {
-  0%,
-  50%,
-  100% {
-    opacity: 1;
-  }
-  25%,
-  75% {
-    opacity: 0;
-  }
-}
-@keyframes blink-1 {
-  0%,
-  50%,
-  100% {
-    opacity: 1;
-  }
-  25%,
-  75% {
-    opacity: 0;
-  }
-}
-
-</style>
-<script>
-	// Uitzetten van f12 bullshit.
-    document.addEventListener('contextmenu', event => event.preventDefault());
- 
-    document.onkeydown = function (e) {
- 
-
-        if(e.keyCode == 123) {
-            return false;
-        }
- 
-
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-            return false;
-        }
-
-        if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-            return false;
-        }
-
-        if(e.ctrlKey && e.keyCode == 85) {
-            return false;
-        }
-    }
- 
-</script>
-<style>
-
-		.reward-container-reward-title-caprihotel {
+		.reward-container-reward-title-websitelevel {
 			text-align: center;
 			
 		}
-		.reward-container-reward-title-caprihotel {
+		.reward-container-reward-title-websitelevel {
 			float: left;
 			padding: 2px;
 			margin: 5px;
@@ -251,7 +21,7 @@ include_once 'includes/header_scripts.php';
 			transition: 0.2s;
 			
 		}
-		.reward-container-reward-title-caprihotel:hover {
+		.reward-container-reward-title-websitelevel:hover {
 			float: left;
 			padding: 2px;
 			margin: 5px;
@@ -265,7 +35,7 @@ include_once 'includes/header_scripts.php';
 <body >
 <?php
 
-		// level Rewards gemaakt door Milan -> Psychology.
+		// level Rewards gemaakt door Milan
 		if(User::userData('ls_experience') < 1000) {
 			$levelShow = 0;
 		} elseif (User::userData('ls_experience') < 2000) {
@@ -407,10 +177,6 @@ include_once 'includes/header_scripts.php';
 <div class="container">
 	<div class="row">
         <div class="col-4">
-		 <div id="shadow-box" style="height: 20px; text-align: center;">
-          <i class="start-icon  fa fa-info-circle faa-shake animated"></i>
-          <strong class="font__weight-semibold">BETA</strong> Functie is nog in programmeer fase.
-        </div>
             <div id="shadow-box">
                 <div style="height: 70px;" class="title-box png20">
                     <div class="title">Jouw level statistieken</div>
@@ -448,49 +214,49 @@ include_once 'includes/header_scripts.php';
                                                  <div class="desc"><font color="white">Hier kan je alle pakketten zien die je kan krijgen of mogelijk kan claimen.</font></div>
                                             </div>
                                 <div class="png20" style="height: 470px; position: relative;">
-                                    <div id="reward-container-caprihotel">
+                                    <div id="reward-container-websitelevel">
 									<form method="POST">
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 										<img style="position: absolute;" src="/templates/yada/img/levelprestige/prestige1.gif" width="40">
 											<p><img src="/templates/yada/img/present.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl5 ?>
 											
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 											<p><img src="/templates/yada/img/present2.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl10 ?>
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 										<img style="position: absolute;" src="/templates/yada/img/levelprestige/prestige2.gif" width="40">
 											<p><img src="/templates/yada/img/present.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl15 ?>
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 											<p><img src="/templates/yada/img/present2.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl20 ?>
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 										<img style="position: absolute;" src="/templates/yada/img/levelprestige/prestige3.gif" width="40">
 											<p><img src="/templates/yada/img/present.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl25 ?>
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 											<p><img src="/templates/yada/img/present2.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl30 ?>
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 										<img style="position: absolute;" src="/templates/yada/img/levelprestige/prestige4.gif" width="40">
 											<p><img src="/templates/yada/img/present.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl35 ?>
 										</div>
-										<div class="reward-container-reward-title-caprihotel">
+										<div class="reward-container-reward-title-websitelevel">
 											<p><img src="/templates/yada/img/present2.gif" width="80"></p>
 											<p><?php echo $config['diamondTen']?> Diamanten</p>
 											<?php echo $buttonlvl40 ?>
